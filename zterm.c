2953c63e42e38c735346f06ffa50fafe6901249c
Zephaniah Loss-Cutler-Hull <warp@aehallh.com>
2022-01-21 11:22:26 -0800

    Move TERM and TERM_PROGRAM to config.
    Now that we can set stuff from the config file, let's just do that by
    default.

--- zterm.c
+++ zterm.c
@@ -1271,8 +1271,6 @@ int main(int argc, char *argv[], char *envp[])
 	terms.active = calloc(terms.n_active, sizeof(*terms.active));
 	terms.active_window = calloc(terms.n_active, sizeof(*terms.active_window));
 
-	setenv("TERM", "xterm-256color", 1);
-	setenv("TERM_PROGRAM", "zterm", 1);
 	chdir(getenv("HOME"));
 
 	{
